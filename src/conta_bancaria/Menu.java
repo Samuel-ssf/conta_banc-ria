8c362e91e9dcc1774b916cbc4950d3179173d486
Samuel <samueldjsferreira@gmail.com>
2025-04-25 19:45:20 -0300

    :hamer: Conta - Bancaria

---
+++ src/conta_bancaria/Menu.java
@@ -0,0 +1,101 @@
+package conta_bancaria;
+
+import java.util.Scanner;
+
+import conta_bancaria.model.Conta;
+import conta_bancaria.util.Cores;
+
+public class Menu {
+
+	public static void main(String[] args) {
+
+		Scanner leia = new Scanner(System.in);
+
+		int opcao;
+
+		Conta c1 = new Conta(1, 123, 1, "Samuel", 50000000);
+		c1.visualizar();
+
+		// Sacar
+		c1.sacar(100);
+		c1.visualizar();
+
+		c1.depositar(1000);
+		c1.visualizar();
+
+		// Alterar o Nome do Titular
+		c1.setTitular("Samuel Silva");
+		c1.visualizar();
+
+		while (true) {
+
+			System.out.println(Cores.TEXT_GREEN + Cores.ANSI_BLACK_BACKGROUND);
+			System.out.println("********************************************************");
+			System.out.println("*                                                      *");
+			System.out.println("*                 BANCO DO BRAZIL COM Z                *");
+			System.out.println("*                                                      *");
+			System.out.println("********************************************************");
+			System.out.println("*                                                      *");
+			System.out.println("*              1 - Criar Conta                         *");
+			System.out.println("*              2 - Listar Todas as Contas              *");
+			System.out.println("*              3 - Buscar Conta por Numero             *");
+			System.out.println("*              4 - Atualizar Dados da Conta            *");
+			System.out.println("*              5 - Apagar Conta                        *");
+			System.out.println("*              6 - Sacar                               *");
+			System.out.println("*              7 - Depositar                           *");
+			System.out.println("*              8 - Transferir Valores Entre Contas     *");
+			System.out.println("*              9 - Sair                                *");
+			System.out.println("*                                                      *");
+			System.out.println("********************************************************");
+			System.out.println("* Entre com a opção desejada:                          *");
+			System.out.println("********************************************************" + Cores.TEXT_RESET);
+
+			opcao = leia.nextInt();
+
+			if (opcao == 9) {
+				System.out.println(Cores.TEXT_GREEN + Cores.ANSI_BLACK_BACKGROUND);
+				System.out.println("********************************************************");
+				System.out.println("Banco do Brazil com Z - O seu Futuro começa aqui!      *");
+				System.out.println("********************************************************");
+				System.out.println(Cores.TEXT_RESET);
+
+				sobre();
+				leia.close();
+				System.exit(0);
+			}
+
+			switch (opcao) {
+			case 1 -> System.out.println(Cores.TEXT_WHITE + "Criar Conta\n\n");
+
+			case 2 -> System.out.println(Cores.TEXT_WHITE + "Listar Todas as Contas\n\n");
+
+			case 3 -> System.out.println(Cores.TEXT_WHITE + "Consultar Dados da Conta - por Número\n\n");
+
+			case 4 -> System.out.println(Cores.TEXT_WHITE + "Atualizar Dados da Conta\n\n");
+
+			case 5 -> System.out.println(Cores.TEXT_WHITE + "Apagar a Conta\n\n");
+
+			case 6 -> System.out.println(Cores.TEXT_WHITE + "Saque\n\n");
+
+			case 7 -> System.out.println(Cores.TEXT_WHITE + "Depósito\n\n");
+
+			case 8 -> System.out.println(Cores.TEXT_WHITE + "Transferência Entre Contas\n\n");
+
+			default -> System.out.println(Cores.TEXT_RED_BOLD + "\nOpção Inválida!\n" + Cores.TEXT_RESET);
+
+			}
+		}
+	}
+
+	public static void sobre() {
+		System.out.println(Cores.TEXT_GREEN + Cores.ANSI_BLACK_BACKGROUND);
+		System.out.println("\n********************************************************");
+		System.out.println("Projeto Desenvolvido por:                              *");
+		System.out.println("Samuel de Jesus Silva - samueldjsferreira@gemail.com   *");
+		System.out.println("https://github.com/Samuel-ssf                          *");
+		System.out.println("********************************************************");
+		System.out.println(Cores.TEXT_RESET);
+
+	}
+
+}
---
+++ src/conta_bancaria/model/Conta.java
@@ -0,0 +1,115 @@
+package conta_bancaria.model;
+
+import java.text.NumberFormat;
+
+import conta_bancaria.util.Cores;
+
+public class Conta {
+
+	// Atributos da Classe
+	private int numero;
+	private int agencia;
+	private int tipo;
+	private String titular;
+	private float saldo;
+
+	// Metodo Construtor
+	public Conta(int numero, int agencia, int tipo, String titular, float saldo) {
+		this.numero = numero;
+		this.agencia = agencia;
+		this.tipo = tipo;
+		this.titular = titular;
+		this.saldo = saldo;
+	}
+
+	// Metodo Get e Set
+	public int getNumero() {
+		return numero;
+	}
+
+	public void setNumero(int numero) {
+		this.numero = numero;
+	}
+
+	public int getAgencia() {
+		return agencia;
+	}
+
+	public void setAgencia(int agencia) {
+		this.agencia = agencia;
+	}
+
+	public int getTipo() {
+		return tipo;
+	}
+
+	public void setTipo(int tipo) {
+		this.tipo = tipo;
+	}
+
+	public String getTitular() {
+		return titular;
+	}
+
+	public void setTitular(String titular) {
+		this.titular = titular;
+	}
+
+	public float getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(float saldo) {
+		this.saldo = saldo;
+	}
+
+	// Metodos Bancários
+
+	public boolean sacar(float valor) {
+		if (this.saldo < valor) {
+			System.out.println("\nSaldo é Insuficiente");
+			return false;
+		}
+
+		this.saldo -= valor;
+		return true;
+
+	}
+
+	public void depositar(float valor) {
+		this.saldo += valor;
+	}
+	
+	// Método para visualizar os dados da conta
+	public void visualizar() {
+
+		NumberFormat nfMoeda = NumberFormat.getCurrencyInstance();
+
+		String tipo = "";
+
+		switch (this.tipo) {
+		case 1 -> tipo = "Conta Corrente";
+		case 2 -> tipo = "Conta Poupança";
+		default -> tipo = "Invalido";
+		}
+		
+		System.out.println(Cores.TEXT_GREEN + Cores.ANSI_BLACK_BACKGROUND);
+		System.out.println("********************************************************");
+		System.out.println("*                   DADOS DA CONTA                     *");
+		System.out.println("********************************************************");
+		System.out.println("" + Cores.TEXT_RESET);
+		System.out.println("* NÚMERO DA CONTA: " + this.numero);
+		System.out.println("* NÚMERO DA AGENCIA: " + this.agencia);
+		System.out.println("* TIPO DA CONTA: " + this.tipo);
+		System.out.println("* TITULAR DA CONTA: " + this.titular);
+		System.out.println("* SALDO DA CONTA: " + nfMoeda.format(this.saldo));
+		System.out.println(Cores.TEXT_GREEN + Cores.ANSI_BLACK_BACKGROUND);
+		System.out.println("********************************************************");
+		System.out.println("" + Cores.TEXT_RESET);
+		System.out.println("\n");
+
+		
+
+	}
+
+}
