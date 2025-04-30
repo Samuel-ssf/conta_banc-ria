be070646f7f9085b364f53ba6614109480c04e6c
Samuel-ssf <samueldjsferreira@gmail.com>
2025-04-30 17:36:02 -0300

    Update ContaPolpanca.java

--- src/conta_bancaria/model/ContaPolpanca.java
+++ src/conta_bancaria/model/ContaPolpanca.java
@@ -1,5 +1,30 @@
 package conta_bancaria.model;
 
-public class ContaPolpanca {
+public class ContaPolpanca extends Conta {
+
+	private int aniversario;
+
+	public ContaPolpanca(int numero, int agencia, int tipo, String titular, float saldo, int aniversario) {
+		super(numero, agencia, tipo, titular, saldo);
+		this.aniversario = aniversario;
+	}
+
+	public int getAniversario() {
+		return aniversario;
+	}
+
+	public void setAniversario(int aniversario) {
+		this.aniversario = aniversario;
+	}
+
+	@Override
+
+	public void visualizar() {
+		super.visualizar();
+		System.out.println("Limite da Conta" + this.getAniversario());
+
+	}
 
 }
+
+
